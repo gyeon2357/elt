8057bbc428b3ee9eb17531bc4dc52359cb73b32d
Park Jeong-Gyeon <78517231+jeonggyeon@users.noreply.github.com>
2023-03-30 14:36:48 +0900

    Add files via upload

--- elt-homepage-alpha/src/views/HomeView.vue
+++ elt-homepage-alpha/src/views/HomeView.vue
@@ -98,68 +98,66 @@ transform="translate(1824.758 968.368) rotate(-135)" style="fill: rgb(33, 33, 33
 </template>
 
 <script>
-// greensock.com, 2023. 03. 27, gsap example
-
 $(document).ready(function () {
   // mouse effect
 
-  // let svgns = 'http://www.w3.org/2000/svg'
-  // let root = document.querySelector('svg')
-  // let ease = 0.7
+  let svgns = 'http://www.w3.org/2000/svg'
+  let root = document.querySelector('svg')
+  let ease = 0.7
 
-  // let pointer = {
-  //   x: window.innerWidth / 2,
-  //   y: window.innerHeight / 2
-  // }
+  let pointer = {
+    x: window.innerWidth / 2,
+    y: window.innerHeight / 2
+  }
 
-  // window.addEventListener('mousemove', (event) => {
-  //   pointer.x = event.clientX
-  //   pointer.y = event.clientY
-  // })
+  window.addEventListener('mousemove', (event) => {
+    pointer.x = event.clientX
+    pointer.y = event.clientY
+  })
 
-  // let leader = (prop) => {
-  //   return prop === 'x' ? pointer.x : pointer.y
-  // }
+  let leader = (prop) => {
+    return prop === 'x' ? pointer.x : pointer.y
+  }
 
-  // let total = 50
-  // for (let i = 0; i < total; i++) {
-  //   leader = createLine(leader, i)
-  // }
+  let total = 50
+  for (let i = 0; i < total; i++) {
+    leader = createLine(leader, i)
+  }
 
-  // function createLine(leader, i) {
-  //   let line = document.createElementNS(svgns, 'line')
-  //   root.appendChild(line)
+  function createLine(leader, i) {
+    let line = document.createElementNS(svgns, 'line')
+    root.appendChild(line)
 
-  //   gsap.set(line, { x: -1500, y: -750 })
+    gsap.set(line, { x: -1500, y: -750 })
 
-  //   let pos = gsap.getProperty(line)
+    let pos = gsap.getProperty(line)
 
-  //   gsap.to(line, {
-  //     duration: 10000,
-  //     x: '+=150',
-  //     y: '+=10',
-  //     repeat: -1,
-  //     ease: 'expo.inOut',
-  //     modifiers: {
-  //       x: () => {
-  //         let posX = pos('x')
-  //         let leaderX = leader('x')
-  //         let x = posX + (leaderX - posX) * ease
-  //         line.setAttribute('x2', leaderX - x)
-  //         return x
-  //       },
-  //       y: () => {
-  //         let posY = pos('y')
-  //         let leaderY = leader('y')
-  //         let y = posY + (leaderY - posY) * ease
-  //         line.setAttribute('y2', leaderY - y)
-  //         return y
-  //       }
-  //     }
-  //   })
+    gsap.to(line, {
+      duration: 10000,
+      x: '+=150',
+      y: '+=10',
+      repeat: -1,
+      ease: 'expo.inOut',
+      modifiers: {
+        x: () => {
+          let posX = pos('x')
+          let leaderX = leader('x')
+          let x = posX + (leaderX - posX) * ease
+          line.setAttribute('x2', leaderX - x)
+          return x
+        },
+        y: () => {
+          let posY = pos('y')
+          let leaderY = leader('y')
+          let y = posY + (leaderY - posY) * ease
+          line.setAttribute('y2', leaderY - y)
+          return y
+        }
+      }
+    })
 
-  //   return pos
-  // }
+    return pos
+  }
 
   // skew example
 
