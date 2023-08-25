d2377704028b2afba8ebbd2e8326f572a645eb79
이세환 <sehwan.lee@medistream.co.kr>
2023-08-25 14:38:02 +0900

    사파리 헤더 메뉴디버그

--- elt-homepage-alpha/src/components/Header.vue
+++ elt-homepage-alpha/src/components/Header.vue
@@ -1,102 +1,90 @@
 <template>
   <header id="header">
-    <h1 id="header-left" class="header-col">
-      <RouterLink to="/" id="home-link" class="header-plane main-title-text" title="Back to home" />
+    <h1 id="header-left"
+        class="header-col">
+      <RouterLink to="/"
+                  id="home-link"
+                  class="header-plane main-title-text"
+                  title="Back to home" />
     </h1>
 
-    <div id="header-center" class="header-col">
+    <div id="header-center"
+         class="header-col">
       <div class="header-menu">
-      <span>Project</span>
-      <span>&lpar;</span>
-      <RouterLink
-        to="/project/list/"
-        id="list-link"
-        class="header-plane header-link main-title-text"
-        >List</RouterLink
-      >
-      <span>&#124;</span>
-      <RouterLink
-        to="/project/gallery/"
-        id="gallery-link"
-        class="header-plane header-link main-title-text"
-        >Gallery</RouterLink
-      >
-      <span>&rpar;</span>
-      <RouterLink
-        to="/weeklyzine/"
-        id="weeklyzine-link"
-        class="header-plane header-link main-title-text"
-        >Weeklyzine</RouterLink
-      >
+        <span>Project</span>
+        <span>&lpar;</span>
+        <RouterLink to="/project/list/"
+                    id="list-link"
+                    class="header-plane header-link main-title-text">List</RouterLink>
+        <span>&#124;</span>
+        <RouterLink to="/project/gallery/"
+                    id="gallery-link"
+                    class="header-plane header-link main-title-text">Gallery</RouterLink>
+        <span>&rpar;</span>
+        <RouterLink to="/weeklyzine/"
+                    id="weeklyzine-link"
+                    class="header-plane header-link main-title-text">Weeklyzine</RouterLink>
       </div>
     </div>
 
-    <div id="header-right" class="header-col">
-      <RouterLink to="/notice/" id="about-link" class="header-plane header-link main-title-text"
-        >Notice</RouterLink
-      >
+    <div id="header-right"
+         class="header-col">
+      <RouterLink to="/notice/"
+                  id="about-link"
+                  class="header-plane header-link main-title-text">Notice</RouterLink>
       <span>|</span>
-      <RouterLink to="/about/" id="about-link" class="header-plane header-link main-title-text"
-        >About</RouterLink
-      >
+      <RouterLink to="/about/"
+                  id="about-link"
+                  class="header-plane header-link main-title-text">About</RouterLink>
     </div>
 
     <div class="mobile-menu">
-      <input id="menu__toggle" type="checkbox" />
-      <label class="menu__btn" for="menu__toggle">
+      <input id="menu__toggle"
+             type="checkbox" />
+      <label class="menu__btn"
+             for="menu__toggle">
         <span></span>
       </label>
 
       <ol class="menu__box">
-        <div id="header-center" class="header-col">
+        <div class="header-col header-center">
           <span>Project:</span>
 
           <div class="mobile__project">
-            <RouterLink
-              to="/project/list/"
-              id="list-link"
-              class="header-plane header-link main-title-text"
-              ><img
-                class="mobile__icon__1"
-                src="/assets/img/icon-list.svg"
-              />List&nbsp;view&nbsp;</RouterLink
-            >
+            <RouterLink to="/project/list/"
+                        id="list-link"
+                        class="header-plane header-link main-title-text"><img class="mobile__icon__1"
+                   src="/assets/img/icon-list.svg" />List&nbsp;view&nbsp;</RouterLink>
           </div>
           <div class="mobile__project">
-            <RouterLink
-              to="/project/gallery/"
-              id="gallery-link"
-              class="header-plane header-link main-title-text"
-              ><img
-                class="mobile__icon__2"
-                src="/assets/img/icon-gallery.svg"
-              />Gallery&nbsp;view&nbsp;</RouterLink
-            >
+            <RouterLink to="/project/gallery/"
+                        id="gallery-link"
+                        class="header-plane header-link main-title-text"><img class="mobile__icon__2"
+                   src="/assets/img/icon-gallery.svg" />Gallery&nbsp;view&nbsp;</RouterLink>
           </div>
         </div>
 
-        <div id="header-center" class="header-col">
-          <RouterLink
-            to="/weeklyzine/"
-            id="weeklyzine-link"
-            class="header-plane header-link main-title-text"
-            >Weeklyzine</RouterLink
-          >
+        <div class="header-col header-center">
+          <RouterLink to="/weeklyzine/"
+                      id="weeklyzine-link"
+                      class="header-plane header-link main-title-text">Weeklyzine</RouterLink>
         </div>
 
-        <div id="header-right" class="header-col">
-          <RouterLink to="/notice/" id="about-link" class="header-plane header-link main-title-text"
-            >Notice</RouterLink
-          >
+        <div id=""
+             class="header-col header-right">
+          <RouterLink to="/notice/"
+                      id="about-link"
+                      class="header-plane header-link main-title-text">Notice</RouterLink>
 
-          <RouterLink to="/about/" id="about-link" class="header-plane header-link main-title-text"
-            >About</RouterLink
-          >
+          <RouterLink to="/about/"
+                      id="about-link"
+                      class="header-plane header-link main-title-text">About</RouterLink>
         </div>
       </ol>
     </div>
 
-    <button id="scroll-to-top" class="main-title-text text-plane">
+    <button id="scroll-to-top"
+            class="main-title-text text-plane">
       <!-- <img id="scroll" fetchpriority="high" src="/assets/img/backtotop.svg" /> -->
       <p id="scroll">top</p>
     </button>
